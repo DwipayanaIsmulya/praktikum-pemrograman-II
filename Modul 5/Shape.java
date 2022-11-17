8358acb8d722d183cb8fef9501e887bc0fec4f61
DwipayanaIsmulya <69786097+DwipayanaIsmulya@users.noreply.github.com>
2022-11-18 01:11:49 +0800

    Add files via upload

---
+++ Modul 5/Shape.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum5.soal1;
+
+/**
+ *
+ * @author ThinkPad L450 i5
+ */
+public abstract class Shape {
+    protected String shapeName;
+    
+    //------------------------------------
+    // Constructor: Sets up the shape.
+    //------------------------------------
+    
+    public Shape(String name) {
+        shapeName = name;
+    }    
+    
+    //--------------------------------------------
+    // Returns the surface area of the shape.
+    //--------------------------------------------
+    
+    protected abstract double area();
+    
+    //------------------------------------
+    // Returns the shape as a String.
+    //------------------------------------
+    
+    public String toString() {
+        return shapeName;
+    }
+}
